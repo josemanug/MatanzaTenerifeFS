74ad831bb4576258bf23388744c505d5d07155a5
jose manuel <josemanu1711@hotmail.com>
2026-08-03 19:54:32 +0100

    Dashboard
    Panel principal para la gestión de jugadores y equipaciones.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import Landing from './components/landing/landing.component.jsx';
 import Login from './components/login/login.component.jsx';
+import Dashboard from './components/dashboard/dashboard.component.jsx';
 
 function Home() {
   return (
@@ -17,6 +18,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+
+        <Route path="/home" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   )
