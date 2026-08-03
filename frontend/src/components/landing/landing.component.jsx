5d9f16dd08c7c5dae47eeffb1b64ea12b70192cb
jose manuel <josemanu1711@hotmail.com>
2026-08-03 19:26:28 +0100

    Mejoras
    Implementación de Bootstrap para estilos y mejoras en el texto a mostrar

--- frontend/src/components/landing/landing.component.jsx
+++ frontend/src/components/landing/landing.component.jsx
@@ -16,6 +16,37 @@ function Landing() {
                         Gestiona jugadores y equipaciones.
                     </p>
 
+                    <section className="row text-center mt-5">
+
+                <div className="col-md-4 mb-4">
+                    <div className="card shadow-sm h-100">
+                        <div className="card-body">
+                            <h2 className="card-title">
+                                Equipaciones
+                            </h2>
+
+                            <p className="card-text">
+                                Gestiona la distribución de equipaciones para los jugadores del equipo, asegurando que todos tengan el uniforme adecuado.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-4 mb-4">
+                    <div className="card shadow-sm h-100">
+                        <div className="card-body">
+                            <h2 className="card-title">
+                                Jugadores
+                            </h2>
+
+                            <p className="card-text">
+                                Gestiona la información de los jugadores del equipo, incluyendo sus datos personales, equipaciones asignadas y categorías.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
                     <div className="d-flex justify-content-center gap-3 mt-4">
                         <Link to="/login" className="btn btn-outline-primary btn-lg">
                             Iniciar sesión
